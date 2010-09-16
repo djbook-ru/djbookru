60390eb97be2472067ff4268762a0ec3432ce9e5
Alerion <alerion.um@gmail.com>
2010-09-16 23:44:09 +0300

    fix

--- src/news/admin.py
+++ src/news/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from news.models import News
+from django.conf import settings 
 
 class NewsAdmin(admin.ModelAdmin):
-    display_fields = ['title']
+    list_display = ['title', 'created']
+
+    #class Media:
+    #    js = [
+    #        settings.ADMIN_MEDIA_PREFIX+'tinymce/jscripts/tiny_mce/tiny_mce.js', 
+    #        'js/tinymce_setup.js'
+    #    ]
     
 admin.site.register(News, NewsAdmin)
