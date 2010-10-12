6919b1d8a2d9e80a71356422a6388fa47ebdebac
Alerion <alerion.um@gmail.com>
2010-10-12 23:45:00 +0300

    add author to news, see README about migration

---
+++ src/news/migrations/0001_initial.py
@@ -0,0 +1,37 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'News'
+        db.create_table('news_news', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('content', self.gf('django.db.models.fields.TextField')()),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('news', ['News'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'News'
+        db.delete_table('news_news')
+
+
+    models = {
+        'news.news': {
+            'Meta': {'ordering': "['-created']", 'object_name': 'News'},
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['news']
