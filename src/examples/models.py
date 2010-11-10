4c3169b4da70f9a9bf66a03cc7236557dd447157
Alerion <alerion.um@gmail.com>
2010-11-10 22:27:20 +0200

    add author for examples

--- src/examples/models.py
+++ src/examples/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from accounts.models import User
 
 class Category(models.Model):
     name = models.CharField(_(u'name'), max_length=255)
@@ -21,7 +22,8 @@ class Example(models.Model):
     title = models.CharField(_(u'title'), max_length=255)
     content = models.TextField(_(u'content'), help_text=_('Use Markdown and HTML'))
     created = models.DateTimeField(_(u'created'), auto_now=True)
-
+    author = models.ForeignKey(User, editable=False)
+    
     class Meta:
         verbose_name = _(u'Example')
         verbose_name_plural = _(u'Examples')
