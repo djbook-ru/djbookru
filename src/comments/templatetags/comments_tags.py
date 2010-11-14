c3b1e4828ea566ef23cf8fcc5c3effbd255fda7a
Alerion <alerion.um@gmail.com>
2010-11-14 18:00:05 +0200

    add last_comments widget

--- src/comments/templatetags/comments_tags.py
+++ src/comments/templatetags/comments_tags.py
@@ -2,8 +2,11 @@ from django import template
 from comments.forms import CommentForm
 from comments.models import Comment
 from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
 
 register = template.Library()
+
+LAST_COMMENTS_COUNT = getattr(settings, 'LAST_COMMENTS_COUNT', 10)
 
 @register.inclusion_tag('comments/form.html', takes_context=True)
 def render_comment_form(context, obj):
@@ -31,3 +34,8 @@ def render_comment_list(context, obj):
 @register.simple_tag    
 def get_comment_count(obj):
     return Comment.get_for_object(obj).count()
+
+@register.inclusion_tag('comments/last_comments.html', takes_context=True)
+def last_comments(context):
+    context['last_comments'] = Comment.objects.order_by('-submit_date')[:LAST_COMMENTS_COUNT]
+    return context
