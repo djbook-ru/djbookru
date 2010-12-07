04d91216eb29376191fa4040b4be88e92e38de6b
Ruslan Popov <ruslan.popov@gmail.com>
2010-12-08 00:38:44 +0300

    Use context to show claims status panel.

--- src/examples/views.py
+++ src/examples/views.py
@@ -1,21 +1,33 @@
-from decorators import render_to
-from examples.models import Category, Example
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
 from django.shortcuts import get_object_or_404
 
-@render_to('examples/index.html')
+from decorators import render_to
+from examples.models import Category, Example
+from claims.models import Claims
+
+def context_processor(request):
+    return {
+        'user': request.user,
+        'debug': settings.DEBUG,
+        'claims': Claims.statistic(),
+        }
+
+@render_to('examples/index.html', context_processor)
 def index(request):
     return {
         'categories': Category.objects.all()
     }
 
-@render_to('examples/category.html')    
+@render_to('examples/category.html', context_processor)
 def category(request, pk):
     obj = get_object_or_404(Category, pk=pk)
     return {
         'category': obj
     }
 
-@render_to('examples/detail.html')      
+@render_to('examples/detail.html', context_processor)
 def detail(request, pk):
     return {
         'obj': get_object_or_404(Example, pk=pk)
--- src/news/views.py
+++ src/news/views.py
@@ -1,19 +1,30 @@
-from django.views.generic.list_detail import object_list
-from news.models import News
+# -*- coding: utf-8 -*-
+
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from django.views.generic.list_detail import object_list
+
 from decorators import render_to
+from news.models import News
+from claims.models import Claims
 
 NEWS_ON_PAGE = getattr(settings, 'NEWS_ON_PAGE', 15)
 
+def context_processor(request):
+    return {
+        'user': request.user,
+        'debug': settings.DEBUG,
+        'claims': Claims.statistic(),
+        }
+
 def index(request):
     qs = News.objects.all()
-    extra_context = {}
-    return object_list(request, qs, NEWS_ON_PAGE, 
-                       template_name='news/index.html', 
+    extra_context = context_processor(request)
+    return object_list(request, qs, NEWS_ON_PAGE,
+                       template_name='news/index.html',
                        extra_context=extra_context)
 
-@render_to('news/news.html')    
+@render_to('news/news.html', context_processor)
 def news(request, pk):
     return {
         'object': get_object_or_404(News, pk=pk)
