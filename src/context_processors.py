ffd9c95c494f9751af5cb11114b1aca16ad1f8ba
Ruslan Popov <ruslan.popov@gmail.com>
2011-02-22 18:28:40 +0300

    i18n

--- src/context_processors.py
+++ src/context_processors.py
@@ -3,7 +3,7 @@ from django import get_version
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 from securelayer import views as securelayer
 from accounts.forms import SSAuth
@@ -19,18 +19,21 @@ def custom(request):
     if reverse('accounts:login') == request.path:
         ready, response, cookie = securelayer.secured_request(
             '/api/', {'service': 'check'})
-        print ready, response, cookie
         if ready and settings.SECURELAYER_USE_WITH_DEBUG_MODE:
             protocol = request.is_secure() and 'https' or 'http'
             host = request.get_host()
             url = reverse('accounts:slogin')
+            form = SSAuth().as_json(
+                caption=ugettext(u'Secured form to enter your credential.'),
+                desc=ugettext(u'We use this service for prevention of interception of your information.'))
             params = {
                 'return_to': '%s://%s%s' % (protocol, host, url,),
                 'next': request.GET.get('next', '/'),
-                'form': SSAuth().as_json(
-                    caption='Secured form to enter your credential.',
-                    desc='We use this service for prevention of interception of your information.'),
+                'form': form,
                 }
+            error_desc = request.session.get('error_desc', None)
+            if error_desc:
+                del request.session['error_desc']
             context.update(
                 {'action': 'http://%s:%s/show/' % (
                     settings.SECURELAYER_HOST,
@@ -38,6 +41,6 @@ def custom(request):
                  'securelayer': True,
                  'redirect_form': securelayer.NextStep(initial={'data': securelayer.sign_this(params)}),
                  'button_list': [{'title': _(u'Login'), 'name': 'login', 'type': 'submit'},],
-                 'error_desc': request.session.get('error_desc', None),
+                 'error_desc': error_desc,
                  } )
     return context
