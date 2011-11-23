21baa3a7b634267ff426245898cdc9419039fbe5
Ruslan Popov <ruslan.popov@gmail.com>
2011-11-24 02:15:37 +0400

    Bug #94: Fixed.

--- src/examples/models.py
+++ src/examples/models.py
@@ -1,4 +1,6 @@
-from django.db import models
+# encoding: utf-8
+
+from django.db import models, transaction
 from django.utils.translation import ugettext_lazy as _
 from accounts.models import User
 from django.core.urlresolvers import reverse
@@ -52,3 +54,28 @@ class Example(models.Model):
     @models.permalink
     def get_edit_url(self):
         return ('admin:examples_example_change', [self.pk])
+
+    @transaction.commit_on_success
+    def save(self):
+        from djangobb_forum.models import Forum, Topic, Post
+        from accounts.models import User
+
+        user = User.objects.get(username='rad')
+        forum = Forum.objects.get(name='Обсуждение рецептов')
+
+        topic = Topic(forum=forum, name=self.title, user=user)
+        topic.save()
+
+        self.topic_id = topic.pk
+
+        super(Example, self).save()
+
+        body_plain = u"""Обсуждение рецепта "%s" (http://djbook.ru%s)."""
+        body_html = u"""Обсуждение рецепта &laquo;<a href="%s">%s</a>&raquo;."""
+        title = self.title
+        url = self.get_absolute_url()
+
+        post = Post(topic=topic, user=user,
+                    body=body_plain % (title, url),
+                    body_html=body_html % (url, title))
+        post.save()
