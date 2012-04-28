8a8fe65adb8bfa1fa6d0111099a857ed98e43544
alerion <alerion.um@gmail.com>
2012-04-28 12:25:10 +0300

    update spelling error send form

--- src/static/js/jquery-spelling.js
+++ src/static/js/jquery-spelling.js
@@ -1,94 +1,126 @@
-var gettext = gettext || function(val){return val}
+var gettext = gettext || function(val){return val;};
 
 $(function() {
-
+    spelling._init();
     $(document).bind('keydown', function(e) {
         if (e.keyCode == 13 && e.ctrlKey) {
             spelling.showForm();
         }
-    })
+    });
 });
 
 var spelling = {
-    /**
-     * Форма для заполнения данных об обнаруженной ошибке
-     */
-    $form: '',
-    showForm: function() {
+    selection: null,
+    $form: $('<div class="modal" id="myModal">'+
+                '<div class="modal-header">'+
+                    '<button class="close" data-dismiss="modal">×</button>'+
+                    '<h3>'+gettext('Send error form')+'</h3>'+
+                '</div>'+
+                '<div class="modal-body">'+
+                    '<h3>'+gettext('Context of error')+'</h3>'+
+                    '<p class="context-error"></p>'+
+                '</div>'+
+                '<form class="form-horizontal">'+
+                    '<fieldset>'+
+                        '<div class="control-group">'+
+                            '<label class="control-label" for="spelling-form-email">E-mail *</label>'+
+                            '<div class="controls"><input id="spelling-form-email" type="text" class="span4" placeholder="E-mail"></div>'+
+                        '</div>'+
+                        '<div class="control-group">'+
+                            '<label class="control-label" for="spelling-form-comment">'+gettext('Comment')+' *</label>'+
+                            '<div class="controls"><textarea id="spelling-form-comment" class="span4" placeholder="'+gettext('Comment')+'"></textarea></div>'+
+                        '</div>'+
+                        '<div class="control-group">'+
+                            '<div class="controls"><label class="checkbox" for="spelling-form-notify"><input id="spelling-form-notify" type="checkbox">'+gettext('Notify me')+'</label></div>'+
+                        '</div>'+
+                    '</fieldset>'+
+                '</form>'+
+                '<div class="modal-footer">'+
+                    '<a href="#" class="btn" data-dismiss="modal">'+gettext('Close')+'</a>'+
+                    '<a href="#" class="btn btn-primary send">'+gettext('Send')+'</a>'+
+                '</div>'+
+            '</div>'),
 
-        var selection = this.getErrorSelection()
+    _init: function(){
+        var $form = this.$form;
+        $form.modal({show: false});
 
-        if (selection == false)
+        $form.on('hide', spelling.reset);
+
+        $('.btn.send', $form).click(function(){
+            spelling.resetFormErrors();
+            var valid = true;
+            var email = $('#spelling-form-email').val();
+            var comment = $('#spelling-form-comment').val();
+
+            if (comment === '') {
+                valid = false;
+                $('#spelling-form-comment').parents('.control-group').addClass('error');
+            }
+
+            if (email === '' || (! spelling.isValidEmail(email))) {
+                 valid = false;
+                $('#spelling-form-email').parents('.control-group').addClass('error');
+            }
+
+            if (valid){
+                $.post('/claims/', {
+                        ctx_left: spelling.selection.context_left,
+                        selected: spelling.selection.context_error,
+                        ctx_right: spelling.selection.context_right,
+                        email: email,
+                        notify: $('#spelling-form-notify').prop('checked'),
+                        comment: comment
+                    },
+                    function(xml) {
+                        var text = $(xml).find('result').text();
+                        if (text == 'ok') {
+                            spelling.alert(gettext('Message sent success!'));
+                        }
+                        else {
+                            spelling.alert(gettext('Something broken. Try later.'));
+                        }
+                    }
+                );
+                $form.modal('hide');
+            }
+
+            return false;
+        });
+    },
+
+    reset: function(){
+        spelling.$form.find('form')[0].reset();
+        spelling.resetFormErrors();
+        spelling.selection = null;
+    },
+
+    resetFormErrors: function(){
+        this.$form.find('.control-group').removeClass('error');
+    },
+
+    showForm: function(){
+        this.selection = this.getErrorSelection();
+
+        if ( ! this.selection)
             return;
 
-        var context_error = '<span>' + selection.context_left + '</span>'
-                + '<span class="ui-widget-content ui-state-error">' + selection.context_error
-                + '</span>' + '<span>' + selection.context_right + '</span>';
+        var context_error = '<span>' + this.selection.context_left + '</span>'+
+            '<span class="selected">' + this.selection.context_error+
+            '</span>' + '<span>' + this.selection.context_right + '</span>';
 
-        this.$form = this.$form || $('<div title="'+gettext('Send error form')+'" >' +
-                '<form id="spelling_form">' +
-                '<p><label>'+gettext('Context of error')+'</label>' +
-                '<span class="display">' + context_error + '</span>' +
-                '</p>' +
-                '<p><label>'+gettext('Comment')+' *</label>' +
-                '<textarea id="comment" />' +
-                '</p>' +
-                '<p><label>E-mail *</label>' +
-                '<input type="text" id="email"></p>' +
-                '<p><input type="checkbox"  id="notify" />' +
-                '<label>'+gettext('Notify about changes of error status')+'</label></p>' +
-                '</div>');
+        $('.context-error', this.$form).html(context_error);
 
-        this.$form.dialog({
-            resizable: false,
-            modal: true,
-            width: 380,
-            beforeClose: function() {
-                $(this).find('form')[0].reset();
-            },
-            buttons: {
-                'Отправить': function() {
-                    var text_errors = '';
-                    if ($('#comment').val() == '') {
-                        text_errors += '<div>'+gettext('Comment is required.')+'</div>';
-                    }
-                    if ($('#email').val() == '') {
-                        text_errors += '<div>'+gettext('E-mail is required.')+'</div>';
-                    }
-                    else if (! spelling.isValidEmail($('#email').val())) {
-                        text_errors += '<div>'+gettext('E-mail is wrong.')+'</div>';
-                    }
+        this.$form.modal('show');
+    },
 
-                    if (text_errors != ''){
-                        spelling.alert(text_errors, gettext('Form has errors.'));
-                        text_errors = '';
-                        return;
-                    }
-
-                    $.post('/claims/', {ctx_left: selection.context_left,
-                        selected: selection.context_error,
-                        ctx_right: selection.context_right,
-                        email: $('#email').val(),
-                        notify: $('#notify').attr('checked'),
-                        comment: $('#comment').val()},
-                          function(xml) {
-                              var text = $(xml).find('result').text();
-                              if (text == 'ok') {
-                                  spelling.alert(gettext('Message sent success!'));
-                                  spelling.updateInformer();
-                              }
-                              else {
-                                  spelling.alert(gettext('Something broken. Try later.'));
-                              }
-
-                          });
-                    $(this).dialog('close');
-                },
-                'Отменить': function() {
-                    $(this).dialog('close');
-                }
-            }
-        });
+    /**
+     * Алерт на замену стандартному
+     * @param message
+     * @param title
+     */
+    alert: function(message) {
+        alert(message);
     },
 
     /**
@@ -117,9 +149,19 @@ var spelling = {
         }
         if (typeof userSelection == 'object') {
             var range = userSelection.getRangeAt(0); // берём первое выделение
-            context_left = range.startContainer.nodeValue.substring(0, range.startOffset);
+            context_left = range.startContainer.nodeValue;
+            if (context_left){
+                context_left = context_left.substring(0, range.startOffset);
+            }else{
+                context_left = '';
+            }
             context_error = userSelection.toString();
-            context_right = range.endContainer.nodeValue.substring(range.endOffset);
+            context_right = range.endContainer.nodeValue;
+            if (context_right){
+                context_right = context_right.substring(0, range.endOffset);
+            }else{
+                context_right = '';
+            }
         } else if (typeof userSelection == 'string') {
             // получаем текст документа (без \n и с нормализованными пробелами)
             bodyText = bodyEl.innerText.replace(/[ \n]+/g, ' ');
@@ -129,7 +171,7 @@ var spelling = {
             var context_start = index;
             var context_end = index + userSelection.length;
             // вычисляем контекст
-            var counter = 5
+            var counter = 5;
             while (counter > 0 && context_start >= 0) {
                 if (bodyText.charAt(context_start - 1) == " ") counter--;
                 context_start--;
@@ -163,36 +205,12 @@ var spelling = {
     },
 
     /**
-     * Алерт на замену стандартному
-     * @param message
-     * @param title
-     */
-    alert: function(message, title) {
-
-        title = title || gettext('Message');
-
-        var $form = $('<div title="' + title + '" >' +
-                '<div>' + message + '</div>' +
-                '</div>');
-
-        $form.dialog({
-            resizable: false,
-            modal: true,
-            width: 300,
-            buttons: {
-                'Ок': function() {
-                    $(this).dialog('close');
-                }
-            }
-        });
-    },
-    /**
      * Валидация email
      * @param value
      */
     isValidEmail: function(value) {
         // contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-        return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(value);
+        return (/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i).test(value);
     }
 
 };
