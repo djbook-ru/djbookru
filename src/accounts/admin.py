b7fb63fc9c3ac3c385b50e4c4afcae260dd0205d
alerion <alerion.um@gmail.com>
2012-08-15 10:49:57 +0300

    fix admin UI

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,23 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
-from models import User as CustomUser, EmailConfirmation, UserAchievement, Achievement
-
-
-class UserAchievementInline(admin.TabularInline):
-    model = UserAchievement
-    extra = 1
+from models import User as CustomUser, EmailConfirmation
 
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_valid_email', 'password')
-    inlines = [UserAchievementInline]
 
-
-class AchievementAdmin(admin.ModelAdmin):
-    pass
 
 admin.site.unregister(User)
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Achievement, AchievementAdmin)
 admin.site.register(EmailConfirmation)
