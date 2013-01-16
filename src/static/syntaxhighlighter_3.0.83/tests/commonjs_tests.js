18e2b9ee754ef036bea6ce2db0cedb78db3b45d4
Ruslan Popov <ruslan.popov@gmail.com>
2013-01-16 20:24:52 +0400

    Fix Git Reqs.

--- src/static/syntaxhighlighter_3.0.83/tests/commonjs_tests.js
+++ src/static/syntaxhighlighter_3.0.83/tests/commonjs_tests.js
@@ -4,49 +4,49 @@
 require.paths.unshift(__dirname + '/../scripts');
 
 var sys = require('sys'),
-	shSyntaxHighlighter = require('shCore').SyntaxHighlighter,
-	code = 'test',
-	brushes = [
-		'AS3',
-		'AppleScript',
-		'Bash',
-		'CSharp',
-		'ColdFusion',
-		'Cpp',
-		'Css',
-		'Delphi',
-		'Diff',
-		'Erlang',
-		'Groovy',
-		'JScript',
-		'Java',
-		'JavaFX',
-		'Perl',
-		'Php',
-		'Plain',
-		'PowerShell',
-		'Python',
-		'Ruby',
-		'Sass',
-		'Scala',
-		'Sql',
-		'Vb',
-		'Xml'
-	]
-	;
+    shSyntaxHighlighter = require('shCore').SyntaxHighlighter,
+    code = 'test',
+    brushes = [
+        'AS3',
+        'AppleScript',
+        'Bash',
+        'CSharp',
+        'ColdFusion',
+        'Cpp',
+        'Css',
+        'Delphi',
+        'Diff',
+        'Erlang',
+        'Groovy',
+        'JScript',
+        'Java',
+        'JavaFX',
+        'Perl',
+        'Php',
+        'Plain',
+        'PowerShell',
+        'Python',
+        'Ruby',
+        'Sass',
+        'Scala',
+        'Sql',
+        'Vb',
+        'Xml'
+    ]
+    ;
 
 brushes.sort();
 
 for (var i = 0; i < brushes.length; i++)
 {
-	var name = brushes[i],
-		brush = require('shBrush' + name).Brush
-		;
-		
-	brush = new brush();
-	brush.init({ toolbar: false });
-	
-	var result = brush.getHtml(code);
-	
-	sys.puts(name + (result != null ? ': ok' : ': NOT OK'));
+    var name = brushes[i],
+        brush = require('shBrush' + name).Brush
+        ;
+
+    brush = new brush();
+    brush.init({ toolbar: false });
+
+    var result = brush.getHtml(code);
+
+    sys.puts(name + (result != null ? ': ok' : ': NOT OK'));
 }
