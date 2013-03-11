b3c15a7a08b885cd0cb5125f0fe4904567624363
Ruslan Popov <ruslan.popov@gmail.com>
2013-03-11 22:31:08 +0400

    update adzone

--- src/dashboard.py
+++ src/dashboard.py
@@ -8,6 +8,8 @@ from django.core.urlresolvers import reverse
 from admin_tools.dashboard import modules, Dashboard, AppIndexDashboard
 from admin_tools.utils import get_admin_site_name
 
+from adzone.modules import AdClickReport
+
 
 class CustomIndexDashboard(Dashboard):
     """
@@ -84,6 +86,7 @@ class CustomIndexDashboard(Dashboard):
                     ])
                 ]))
 
+        self.children.append(AdClickReport(_('AdClick Report')))
         self.children.append(modules.RecentActions(_('Recent Actions'), 15))
 
 
