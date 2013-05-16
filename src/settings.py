6b38c34d1f15a792c7382c66a92004871db63d4e
Alerion <alerion.um@gmail.com>
2013-05-16 13:05:27 +0300

    remove old templates

--- src/settings.py
+++ src/settings.py
@@ -153,7 +153,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    rel_project('templates'),
+    rel_project('newtemplates'),
 )
 
 FIXTURE_DIRS = (
