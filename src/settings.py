a9ca01a12856b5f7eee41d0cc90dc13152106c77
Alerion <alerion.um@gmail.com>
2013-05-16 13:07:03 +0300

    rename newtemplates to templates

--- src/settings.py
+++ src/settings.py
@@ -153,7 +153,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    rel_project('newtemplates'),
+    rel_project('templates'),
 )
 
 FIXTURE_DIRS = (
