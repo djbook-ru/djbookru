eb8c86a466a6a0a9bb413f8e16b93de2b21a73b0
Alerion <alerion.um@gmail.com>
2013-05-31 13:25:35 +0300

    update sorl

--- lib/sorl/thumbnail/fields.py
+++ lib/sorl/thumbnail/fields.py
@@ -1,231 +1,72 @@
-import os.path
-from UserDict import DictMixin
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
-
-from django.db.models.fields.files import ImageField, ImageFieldFile
-from django.core.files.base import ContentFile
-from django.utils.safestring import mark_safe
-from django.utils.functional import curry
-from django.utils.html import escape
-from django.conf import settings
-
-from sorl.thumbnail.base import Thumbnail
-from sorl.thumbnail.main import DjangoThumbnail, build_thumbnail_name
-from sorl.thumbnail.utils import delete_thumbnails
+from __future__ import with_statement
+from django.db import models
+from django.db.models import Q
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from sorl.thumbnail import default
 
 
-REQUIRED_ARGS = ('size',)
-ALL_ARGS = {
-    'size': 'requested_size',
-    'options': 'opts',
-    'quality': 'quality',
-    'basedir': 'basedir',
-    'subdir': 'subdir',
-    'prefix': 'prefix',
-    'extension': 'extension',
-}
-BASE_ARGS = {
-    'size': 'requested_size',
-    'options': 'opts',
-    'quality': 'quality',
-}
-TAG_HTML = '<img src="%(src)s" width="%(width)s" height="%(height)s" alt="" />'
+__all__ = ('ImageField', 'ImageFormField')
 
 
-class ThumbsDict(object, DictMixin):
-    def __init__(self, descriptor):
-        super(ThumbsDict, self).__init__()
-        self.descriptor = descriptor
+class ImageField(models.FileField):
+    def delete_file(self, instance, sender, **kwargs):
+        """
+        Adds deletion of thumbnails and key kalue store references to the
+        parent class implementation. Only called in Django < 1.2.5
+        """
+        file_ = getattr(instance, self.attname)
+        # If no other object of this type references the file, and it's not the
+        # default value for future objects, delete it from the backend.
+        query = Q(**{self.name: file_.name}) & ~Q(pk=instance.pk)
+        qs = sender._default_manager.filter(query)
+        if (file_ and file_.name != self.default and not qs):
+            default.backend.delete(file_)
+        elif file_:
+            # Otherwise, just close the file, so it doesn't tie up resources.
+            file_.close()
 
-    def keys(self):
-        return self.descriptor.field.extra_thumbnails.keys()
+    def formfield(self, **kwargs):
+        defaults = {'form_class': ImageFormField}
+        defaults.update(kwargs)
+        return super(ImageField, self).formfield(**defaults)
 
-
-class LazyThumbs(ThumbsDict):
-    def __init__(self, *args, **kwargs):
-        super(LazyThumbs, self).__init__(*args, **kwargs)
-        self.cached = {}
-
-    def __getitem__(self, key):
-        thumb = self.cached.get(key)
-        if not thumb:
-            args = self.descriptor.field.extra_thumbnails[key]
-            thumb = self.descriptor._build_thumbnail(args)
-            self.cached[key] = thumb
-        return thumb
-
-    def keys(self):
-        return self.descriptor.field.extra_thumbnails.keys()
-
-
-class ThumbTags(ThumbsDict):
-    def __getitem__(self, key):
-        thumb = self.descriptor.extra_thumbnails[key]
-        return self.descriptor._build_thumbnail_tag(thumb)
-
-
-class BaseThumbnailFieldFile(ImageFieldFile):
-    def _build_thumbnail(self, args):
-        # Build the DjangoThumbnail kwargs.
-        kwargs = {}
-        for k, v in args.items():
-            kwargs[ALL_ARGS[k]] = v
-        # Build the destination filename and return the thumbnail.
-        name_kwargs = {}
-        for key in ['size', 'options', 'quality', 'basedir', 'subdir',
-                    'prefix', 'extension']:
-            name_kwargs[key] = args.get(key)
-        source = getattr(self.instance, self.field.name)
-        dest = build_thumbnail_name(source.name, **name_kwargs)
-        return DjangoThumbnail(source, relative_dest=dest, **kwargs)
-
-    def _build_thumbnail_tag(self, thumb):
-        opts = dict(src=escape(thumb), width=thumb.width(),
-                    height=thumb.height())
-        return mark_safe(self.field.thumbnail_tag % opts)
-
-    def _get_extra_thumbnails(self):
-        if self.field.extra_thumbnails is None:
-            return None
-        if not hasattr(self, '_extra_thumbnails'):
-            self._extra_thumbnails = LazyThumbs(self)
-        return self._extra_thumbnails
-    extra_thumbnails = property(_get_extra_thumbnails)
-
-    def _get_extra_thumbnails_tag(self):
-        if self.field.extra_thumbnails is None:
-            return None
-        return ThumbTags(self)
-    extra_thumbnails_tag = property(_get_extra_thumbnails_tag)
-
-    def save(self, *args, **kwargs):
-        # Optionally generate the thumbnails after the image is saved.
-        super(BaseThumbnailFieldFile, self).save(*args, **kwargs)
-        if self.field.generate_on_save:
-            self.generate_thumbnails()
-
-    def delete(self, *args, **kwargs):
-        # Delete any thumbnails too (and not just ones defined here in case
-        # the {% thumbnail %} tag was used or the thumbnail sizes changed).
-        relative_source_path = getattr(self.instance, self.field.name).name
-        delete_thumbnails(relative_source_path)
-        super(BaseThumbnailFieldFile, self).delete(*args, **kwargs)
-
-    def generate_thumbnails(self):
-        # Getting the thumbs generates them.
-        if self.extra_thumbnails:
-            self.extra_thumbnails.values()
-
-
-class ImageWithThumbnailsFieldFile(BaseThumbnailFieldFile):
-    def _get_thumbnail(self):
-        return self._build_thumbnail(self.field.thumbnail)
-    thumbnail = property(_get_thumbnail)
-
-    def _get_thumbnail_tag(self):
-        return self._build_thumbnail_tag(self.thumbnail)
-    thumbnail_tag = property(_get_thumbnail_tag)
-
-    def generate_thumbnails(self, *args, **kwargs):
-        self.thumbnail.generate()
-        Super = super(ImageWithThumbnailsFieldFile, self)
-        return Super.generate_thumbnails(*args, **kwargs)
-
-
-class ThumbnailFieldFile(BaseThumbnailFieldFile):
-    def save(self, name, content, *args, **kwargs):
-        new_content = StringIO()
-        # Build the Thumbnail kwargs.
-        thumbnail_kwargs = {}
-        for k, argk in BASE_ARGS.items():
-            if not k in self.field.thumbnail:
-                continue
-            thumbnail_kwargs[argk] = self.field.thumbnail[k]
-        Thumbnail(source=content, dest=new_content, **thumbnail_kwargs)
-        new_content = ContentFile(new_content.read())
-        super(ThumbnailFieldFile, self).save(name, new_content, *args,
-                                             **kwargs)
-
-    def _get_thumbnail_tag(self):
-        opts = dict(src=escape(self.url), width=self.width,
-                    height=self.height)
-        return mark_safe(self.field.thumbnail_tag % opts)
-    thumbnail_tag = property(_get_thumbnail_tag)
-
-
-class BaseThumbnailField(ImageField):
-    def __init__(self, *args, **kwargs):
-        # The new arguments for this field aren't explicitly defined so that
-        # users can still use normal ImageField positional arguments.
-        self.extra_thumbnails = kwargs.pop('extra_thumbnails', None)
-        self.thumbnail_tag = kwargs.pop('thumbnail_tag', TAG_HTML)
-        self.generate_on_save = kwargs.pop('generate_on_save', False)
-
-        super(BaseThumbnailField, self).__init__(*args, **kwargs)
-        _verify_thumbnail_attrs(self.thumbnail)
-        if self.extra_thumbnails:
-            for extra, attrs in self.extra_thumbnails.items():
-                name = "%r of 'extra_thumbnails'"
-                _verify_thumbnail_attrs(attrs, name)
+    def save_form_data(self, instance, data):
+        if data is not None:
+            setattr(instance, self.name, data or '')
 
     def south_field_triple(self):
-        """
-        Return a suitable description of this field for South.
-        """
-        # We'll just introspect ourselves, since we inherit.
         from south.modelsinspector import introspector
-        field_class = "django.db.models.fields.files.ImageField"
+        cls_name = '%s.%s' % (self.__class__.__module__ , self.__class__.__name__)
         args, kwargs = introspector(self)
-        # That's our definition!
-        return (field_class, args, kwargs)
+        return (cls_name, args, kwargs)
 
 
-class ImageWithThumbnailsField(BaseThumbnailField):
-    """
-    photo = ImageWithThumbnailsField(
-        upload_to='uploads',
-        thumbnail={'size': (80, 80), 'options': ('crop', 'upscale'),
-                   'extension': 'png'},
-        extra_thumbnails={
-            'admin': {'size': (70, 50), 'options': ('sharpen',)},
-        }
-    )
-    """
-    attr_class = ImageWithThumbnailsFieldFile
+class ImageFormField(forms.FileField):
+    default_error_messages = {
+        'invalid_image': _(u"Upload a valid image. The file you uploaded was "
+                           u"either not an image or a corrupted image."),
+    }
 
-    def __init__(self, *args, **kwargs):
-        self.thumbnail = kwargs.pop('thumbnail', None)
-        super(ImageWithThumbnailsField, self).__init__(*args, **kwargs)
+    def to_python(self, data):
+        """
+        Checks that the file-upload field data contains a valid image (GIF,
+        JPG, PNG, possibly others -- whatever the engine supports).
+        """
+        f = super(ImageFormField, self).to_python(data)
+        if f is None:
+            return None
+        # We need to get a file raw data to validate it.
+        if hasattr(data, 'temporary_file_path'):
+            with open(data.temporary_file_path(), 'rb') as fp:
+                raw_data = fp.read()
+        elif hasattr(data, 'read'):
+            raw_data = data.read()
+        else:
+            raw_data = data['content']
+        if not default.engine.is_valid_image(raw_data):
+            raise forms.ValidationError(self.error_messages['invalid_image'])
+        if hasattr(f, 'seek') and callable(f.seek):
+            f.seek(0)
+        return f
 
-
-class ThumbnailField(BaseThumbnailField):
-    """
-    avatar = ThumbnailField(
-        upload_to='uploads',
-        size=(200, 200),
-        options=('crop',),
-        extra_thumbnails={
-            'admin': {'size': (70, 50), 'options': (crop, 'sharpen')},
-        }
-    )
-    """
-    attr_class = ThumbnailFieldFile
-
-    def __init__(self, *args, **kwargs):
-        self.thumbnail = {}
-        for attr in ALL_ARGS:
-            if attr in kwargs:
-                self.thumbnail[attr] = kwargs.pop(attr)
-        super(ThumbnailField, self).__init__(*args, **kwargs)
-
-
-def _verify_thumbnail_attrs(attrs, name="'thumbnail'"):
-    for arg in REQUIRED_ARGS:
-        if arg not in attrs:
-            raise TypeError('Required attr %r missing in %s arg' % (arg, name))
-    for attr in attrs:
-        if attr not in ALL_ARGS:
-            raise TypeError('Invalid attr %r found in %s arg' % (arg, name))
--- lib/sorl/thumbnail/models.py
+++ lib/sorl/thumbnail/models.py
@@ -1,1 +1,10 @@
-# Needs a models.py file so that tests are picked up.
+from django.db import models
+from sorl.thumbnail.conf import settings
+
+
+class KVStore(models.Model):
+    key = models.CharField(max_length=200, primary_key=True,
+        db_column=settings.THUMBNAIL_KEY_DBCOLUMN
+        )
+    value = models.TextField()
+
