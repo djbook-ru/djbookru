b29c03d287274fcd2e430009af7d26a6f150b5d1
Alerion <alerion.um@gmail.com>
2013-08-21 12:58:44 +0300

    update admin UI for doc_comments

--- src/doc_comments/admin.py
+++ src/doc_comments/admin.py
@@ -7,6 +7,7 @@ from . import models
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['created', 'comment', 'url', 'author', 'status']
     list_filter = ['status']
+    actions = ['close']
 
     def url(self, obj):
         return '<a href="%s">%s</a>' % (obj.get_absolute_url(), obj.page_title)
@@ -15,5 +16,9 @@ class CommentAdmin(admin.ModelAdmin):
     def comment(self, obj):
         return obj.get_content()
     comment.allow_tags = True
+
+    def close(self, request, queryset):
+        queryset.update(status=models.Comment.CLOSED)
+    close.short_description = "Close comment"
 
 admin.site.register(models.Comment, CommentAdmin)
