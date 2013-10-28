44dbb395003ad2ab6ad7440f91163b254ccb307e
Maltor <vnmalyna@gmail.com>
2013-10-28 21:25:40 +0200

    Re #191: Add button for edit news.

--- src/news/urls.py
+++ src/news/urls.py
@@ -7,5 +7,6 @@ from . import feeds
 urlpatterns = patterns('src.news.views',
     url(r'^$', 'index', name='index'),
     url(r'^(?P<pk>\d+)/$', 'news', name='news'),
+    url(r'^(?P<pk>\d+)/edit/$', 'edit', name='edit'),
     url(r'^feed/$', feeds.LatestNewsFeed(), name='rss'),
 )
--- src/news/views.py
+++ src/news/views.py
@@ -3,6 +3,7 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.views.generic.list_detail import object_list
+from django.views.decorators.csrf import csrf_exempt
 
 from .. decorators import render_to
 from . import models
@@ -21,3 +22,23 @@ def news(request, pk):
     return {
         'obj': get_object_or_404(models.News, pk=pk)
     }
+    
+    
+@csrf_exempt
+@render_to('news/edit.html')
+def edit(request, pk):
+    obj = get_object_or_404(models.News, pk=pk)
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            form = models.NewsForm(request.POST, instance=obj)
+            if form.is_valid():
+                form.save()
+                return news(request, obj.id)
+        else:
+            form = models.NewsForm(instance=obj)
+        return {
+            'obj': obj,
+            'form': form
+        }
+    else:
+       return news(request, obj.id) 
