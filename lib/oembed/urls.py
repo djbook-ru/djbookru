502798e246c1e9d0457a8b70cee1b42f4172aa9b
Ruslan Popov <ruslan.popov@gmail.com>
2015-03-06 19:58:18 +0300

    Remove legacy code.

--- lib/oembed/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('oembed.views',
-    url(r'^$', 'oembed_schema', name='oembed_schema'),
-    url(r'^json/$', 'json', name='oembed_json'),
-    url(r'^consume/json/$', 'consume_json', name='oembed_consume_json'),
-)
