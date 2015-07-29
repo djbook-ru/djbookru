2c1ff9bf2ae1c6ffe5abfd8bcd66abbaa4971f19
Alexander Tyapkin <atyapkin20@gmail.com>
2015-07-29 23:12:34 +0300

    Added url patterns, create test view, create templates for app jobs

--- src/jobs/views.py
+++ src/jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Jobs
+
+
+def index(request):
+    obj = Jobs.objects.published_jobs()
+    return render(request, 'jobs/index.html', {'objects': obj})
