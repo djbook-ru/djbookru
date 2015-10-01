977c6ffacfa691391fbe343b18c5bc4e552b9d6d
Ruslan Popov <ruslan.popov@gmail.com>
2015-10-01 09:48:00 +0300

    Remove annoying message about TEMPLATE_DEBUG.

--- src/settings.py
+++ src/settings.py
@@ -104,6 +104,7 @@ TEMPLATES = [
         ],
         'APP_DIRS': True,
         'OPTIONS': {
+            'debug': DEBUG,
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
