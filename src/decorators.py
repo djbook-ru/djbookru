c814d27818fdb5022ec1624a2019fa81cfc0dacc
alkalit <lyoffa412@gmail.com>
2015-11-18 13:56:41 +0500

    add some docs

--- src/decorators.py
+++ src/decorators.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json
 from functools import update_wrapper
 
@@ -7,26 +8,46 @@ from django.http import HttpResponse
 from django.core.serializers.json import DjangoJSONEncoder
 
 
-def render_to(template, processor=None):
+def render_to(template_name, processor=None):
+
+    """
+    Превращает функцию во вьюху. Требует чтобы та вернула контекст в виде словаря.
+
+    template_name - имя шаблона.
+    processor - context processor. Дополнительный процессор.
+    """
+
     def renderer(func):
         def wrapper(request, *args, **kw):
+
             if processor is not None:
                 ctx_proc = RequestContext(request, processors=[processor])
             else:
                 ctx_proc = RequestContext(request)
+
             output = func(request, *args, **kw)
+
             if isinstance(output, (list, tuple)):
                 return render_to_response(output[1], output[0], ctx_proc)
             elif isinstance(output, dict):
-                return render_to_response(template, output, ctx_proc)
+                return render_to_response(template_name, output, ctx_proc)
+
             return output
         return wrapper
     return renderer
 
 
 def render_to_json(func):
+
+    """
+    Превращает функцию во вьюху. Пытается преобразовать ее возвращаемое значение и вернуть json.
+    """
+
     def wrapper(request, *args, **kwargs):
+
         result = func(request, *args, **kwargs)
         data = json.dumps(result, cls=DjangoJSONEncoder)
+
         return HttpResponse(data, content_type="application/json")
+
     return update_wrapper(wrapper, func)
