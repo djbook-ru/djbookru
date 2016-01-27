78582d2cf529539b59f558fcbe9b233b52114a76
Alexander Tyapkin <atyapkin20@gmail.com>
2016-01-27 15:45:09 +0200

    iss#157 TODO added

--- src/jobs/urls.py
+++ src/jobs/urls.py
@@ -7,9 +7,13 @@ from src.jobs.views import JobsListView, CompanyAllVacanciesListView, JobDetailV
 urlpatterns = [
     url(r'^$', JobsListView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', JobDetailView.as_view(), name="job_detail"),
-    url(r'^company/(?P<company>[%a-zA-Z0-9]+)/$', CompanyAllVacanciesListView.as_view(),
+    url(r'^company/(?P<company>[%a-zA-Z0-9]+)/$',
+        CompanyAllVacanciesListView.as_view(),
         name='all_vacancies_company'),
     url(r'^add_position', 'src.jobs.views.add_position', name='add_position'),
-    url(r'^edit_position/(?P<pk>\d+)/$', 'src.jobs.views.edit_position',
+    url(r'^edit_position/(?P<pk>\d+)/$',
+        'src.jobs.views.edit_position',
         name='edit_position'),
 ]
+
+# TODO: all_vacancies_company URL does not work when there are Cyrillic characters
